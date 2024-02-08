333bc188647bc56ad0cb0a3f72f77b5d71e193bd
Priti Rai <138751304+Pritiprty@users.noreply.github.com>
2024-02-08 19:48:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-var dateDiffInDays = function (date1, date2) {
-  //   write your code here
-};
+
+function dateDiffInDays(date1, date2) {
+    // Convert input strings to Date objects
+    const d1 = new Date(date1);
+    const d2 = new Date(date2);
+
+    // Calculate the difference in milliseconds
+    const diffInMs = d2 - d1;
+
+    // Convert milliseconds to days
+    const diffInDays = Math.floor(diffInMs / (1000 * 60 * 60 * 24));
+
+    return diffInDays;
+}
+
 
 // Do not change the code below.
 const dateOne = prompt("Enter Start Date.");
